e0e03e4b54fd23e2796d67a846be079fd0aeba15
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2025-01-24 21:18:39 +1100

    refactored for mobile

--- src/components/sidebar/MobileNavigation.tsx
+++ src/components/sidebar/MobileNavigation.tsx
@@ -131,8 +131,7 @@ const MobileNavigation: React.FC = () => {
   };
 
   return (
-    // <div className="fixed bottom-0 left-0 right-0 block md:hidden z-50">
-    <div className="block md:hidden z-50">
+    <div className="block md:hidden z-50 h-full w-full">
       <div className="flex items-center justify-between p-2 bg-transparent backdrop-blur-md">
         <div className="flex space-x-6 justify-center flex-1">
           {items.map((item: MenuItem) => (
